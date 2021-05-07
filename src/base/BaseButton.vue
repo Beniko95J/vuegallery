b13a486171581eea8e6e9a1a3f9a53ef54dbbc92
Beniko95J <beniko_j@hotmail.com>
2021-05-07 23:34:35 +0900

    add base button

---
+++ src/base/BaseButton.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="base-button">
+    <router-link :to="to" class="router-link">
+      <div class="flex-layout">
+        <div class="flex-item">
+          <span>
+            {{ buttonName }}
+          </span>
+        </div>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String
+    },
+    buttonName: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.base-button {
+  height: 48px;
+  min-width: 60px;
+  border-radius: 24px;
+  box-sizing: border-box;
+  .router-link {
+    display: block;
+    width: 100%;
+    height: 100%;
+    border-radius: 24px;
+    cursor: pointer;
+    color: #333;
+    .flex-layout {
+      width: 100%;
+      height: 100%;
+      border-radius: 24px;
+      background-color: #111;
+      box-sizing: border-box;
+      padding-left: 16px;
+      padding-right: 16px;
+      .flex-item {
+        display: flex;
+        width: 100%;
+        height: 100%;
+        justify-content: center;
+        align-items: center;
+        flex-direction: row;
+        span {
+          font-size: 16px;
+          text-align: center;
+          font-weight: 700;
+          font-family: "Segoe UI", "Fira Sans", "Droid Sans", "Helvetica Neue", Helvetica, "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", "ＭＳ Ｐゴシック", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/gallery-list/gallery-list.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="gallery-list">
+    <div class="gallery-list-head">
+      <base-button to="/home" buttonName="Home" class="base-button"></base-button>
+      <div class="search-head">
+        <input type="text" placeholder="Search" class="search-input">
+      </div>
+    </div>
+    
+    <div class="gallery-list-main">
+    </div>
+
+    <div class="bg" :style="{backgroundImage: `url(${picUrl})`}"></div>
+    <div class="mask"></div>
+  </div>
+</template>
+
+<script>
+import BaseButton from '@/base/BaseButton'
+
+export default {
+  components: {
+    BaseButton
+  },
+  data() {
+    return {
+      picUrl: require('@/assets/wallpaper/wp.jpg')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.gallery-list {
+  .gallery-list-head {
+    display: flex;
+    width: 100%;
+    height: 80px;
+    box-sizing: border-box;
+    padding: 10px;
+    .base-button {
+      
+    }
+    .search-head {
+      display: flex;
+      width: 100%;
+      height: 40px;
+      padding: 10px 15px;
+      overflow: hidden;
+      background: @search_bg_coloe;
+      .search-input {
+        flex: 1;
+        height: 40px;
+        box-sizing: border-box;
+        padding: 0 15px;
+        border: 1px solid @btn_color;
+        outline: 0;
+        background: transparent;
+        color: @text_color_active;
+        font-size: @font_size_medium;
+        box-shadow: 0 0 1px 0 #fff inset;
+        &::placeholder {
+          color: @text_color
+        }
+      }
+    }
+  }
+
+  .mask,
+  .bg {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+  }
+
+  .mask {
+    z-index: -1;
+    background-color: @mask_color;
+  }
+
+  .bg {
+    z-index: -2;
+    background-repeat: no-repeat;
+    background-size: cover;
+    filter: blur(5px);
+    opacity: 0.7;
+    transition: 1s;
+  }
+}
+</style>
